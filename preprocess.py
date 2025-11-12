d2d392c8ce23783258ea6e2ef92d1ec98f001e4c
seanl26 <seanl26@uw.edu>
2025-11-11 16:21:53 -0800

    made code to output data csv with selected columns

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,5 @@
 import json
-# import csv
+import csv
 # import pandas as pd
 # import os
 
@@ -29,21 +29,38 @@ data = data['recipes']
 for category in recipe_categories:
     # each category has a subcategory ("quick lunch recipes", etc.)
     sub_category = data[category]
-    for sub, recipes in sub_category.items():
+    for _, recipes in sub_category.items():
         # columns we want in our new category
         # we should save and export the columns we want this into a new csv
         for recipe in recipes:
             counter += 1
-            recipes_array.append({
-                "id": recipe["id"],
-                "name": recipe.get("name", ""),
-                "ingredients": recipe.get("ingredients", []),
-                "steps": recipe.get("steps", []),
-                "description": recipe.get("description", ""),
-                "times": recipe.get("times", {}),
-                "difficulty": recipe.get("difficulty", ""),
-            })
+            # recipes_array.append({
+            #     "id": recipe["id"],
+            #     "name": recipe.get("name", ""),
+            #     "ingredients": recipe.get("ingredients", []),
+            #     "steps": recipe.get("steps", []),
+            #     "description": recipe.get("description", ""),
+            #     "times": recipe.get("times", {}),
+            #     "difficulty": recipe.get("difficulty", ""),
+            # })
+            description = recipe.get('description', '')
+            ingredients = ",".join(recipe.get('ingredients', []))
+            steps = recipe.get("steps", [])
+            prep_time = recipe.get('times', {}).get('preparation', '')
+            cook_time = recipe.get('times', {}).get('cooking', '')
+            difficult = recipe.get('difficult', '')
+            text = f"{recipe.get('name', '')}. {description}. {ingredients}.\n"
+            text += f"{steps}. Prep Time: {prep_time} Cook Time: {cook_time}."
+            text += f"\nDifficulty: {difficult}."
+            recipes_array.append([recipe["id"], text])
             if counter > 500:
                 break
 
-print(recipes_array)
+headers = ["id", "text"]
+
+with open('output.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(headers)
+    writer.writerows(recipes_array)
+
+# print(recipes_array)
